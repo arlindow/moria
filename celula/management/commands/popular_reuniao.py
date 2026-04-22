4a97770de7d265004ab4de017b44c4c010820e08
Arlindo Wilson Dos Santos <109003843+arlindow@users.noreply.github.com>
2026-04-22 03:08:19 +0000

    ajuste

--- celula/management/commands/popular_reuniao.py
+++ celula/management/commands/popular_reuniao.py
@@ -1,36 +1,22 @@
 from django.core.management.base import BaseCommand
 from celula.models import Reuniao
-from datetime import date
+from datetime import datetime
 
 
 class Command(BaseCommand):
-    help = 'Cadastra próximas reuniões'
+    help = 'Cadastrar reunião'
 
     def handle(self, *args, **kwargs):
 
-        reunioes = [
-            {
+        reuniao, created = Reuniao.objects.get_or_create(
+            data=datetime(2026, 4, 23, 20, 0),
+            defaults={
                 'titulo': 'Célula Moriá',
-                'tema': 'Próxima Reunião',
-                'data': date(2026, 4, 23),
-                'hora': '20:00',
                 'local': 'Residência de Tiago e Juliana',
-            },
-        ]
+            }
+        )
 
-        for r in reunioes:
-            obj, created = Reuniao.objects.get_or_create(
-                data=r['data'],
-                defaults=r
-            )
-
-            if created:
-                self.stdout.write(self.style.SUCCESS(
-                    f"Reunião criada para {r['data']}"
-                ))
-            else:
-                self.stdout.write(
-                    f"Já existe reunião em {r['data']}"
-                )
-
-        self.stdout.write(self.style.SUCCESS("Finalizado."))
+        if created:
+            self.stdout.write(self.style.SUCCESS("Reunião criada"))
+        else:
+            self.stdout.write("Reunião já existe")
